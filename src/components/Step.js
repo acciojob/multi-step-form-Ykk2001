ef008e2106423c5a15208fb9d02cc2227b9b4aa4
Ykk2001 <yogeshkhatake18@gmail.com>
2025-11-29 13:30:41 +0530

    Done2

--- src/components/Step.js
+++ src/components/Step.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react"
+export default function Step({step,formData,handleChange,nextStep,prevStep,handleSubmit})
+{
+  return (<>
+      <div className="card">
 
-export default function Step() {
-  return (
-    <div>
-      
-    </div>
-  )
+        <h2>Step:{step}</h2>
+
+       {step==1 && (<div id="step1">
+          <label>First Name:</label>
+          <input id="first_name" value={formData.first_name} onChange={(e)=>handleChange(e)}></input>
+           <br></br> <br></br>
+          <label>Last Name:</label>
+          <input id="last_name" value={formData.last_name} onChange={(e)=>handleChange(e)}></input>
+       </div>)}
+
+       {step==2 && (<div id="step2">
+          <label>Car Model:</label>
+          <input id="model" value={formData.model} onChange={(e)=>handleChange(e)}></input>
+           <br></br> <br></br>
+          <label>Car Price:</label>
+          <input id="car_price" value={formData.car_price} onChange={(e)=>handleChange(e)}></input>
+       </div>)}
+
+       {step==3 && (<div id="step3">
+          <label>Card Info:</label>
+          <input id="card_info" value={formData.card_info} onChange={(e)=>handleChange(e)}></input>
+           <br></br> <br></br>
+          <label>Expiry Date:</label>
+          <input id="expiry_date" value={formData.expiry_date} onChange={(e)=>handleChange(e)}></input>
+       </div>)}
+         
+         <br></br> <br></br>
+
+       <div className="buttons">
+            {step>1 && <button onClick={prevStep}>Previous</button>}{' '}
+            {step<3 && <button onClick={nextStep}>Next</button>}
+            {step==3 && <button onClick={handleSubmit}>Submit</button>}
+       </div>
+
+      </div>
+       
+  </>)
 }
